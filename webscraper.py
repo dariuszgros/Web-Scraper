d4395e26acbcc59be91553e74496eb0093321423
Dariusz Gros <dariusz.gros@gmail.com>
2023-10-13 21:02:39 +0200

    Added more versatility, permission to select an element or attribute. Removed the need to add http/https for web pages.

--- webscraper.py
+++ webscraper.py
@@ -1,28 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urlunparse
 
 # Define the URL of the website we want to scrape
-url = input("Enter the URL of the website you want to scrape: ")
+user_url = input("Enter the URL of the website you want to scrape: ")
+
+# Ensuring the URL starts with 'http://' or 'https://
+parsed_url = urlparse(user_url)
+if not parsed_url.scheme:
+    user_url = urlunparse(("http", user_url, "", "", "", ""))
 
 # Let the user define the file name for a scraped data
 file_name = input("Enter the name of the file to save the data: ")
 
+# Ask user for the HTML element or atribute to scrape
+element = input("Enter the element you want to scrape (like 'p', 'div', 'h1' etc.): ")
+attribute = input("Enter the name ot the attribute you want o scrape (like 'class', 'id' etc.): ")
+
 # Sending an HTTP GET request to the website
-response = requests.get(url)
+response = requests.get(user_url)
 
 if response.status_code == 200:
     # Parsing the HTML content
     soup = BeautifulSoup(response.text, "html.parser")
 
     # Finding and extracting the quotes
-    quotes = soup.find_all("span", class_="text")
+    selected_elements = soup.find_all(element, class_=attribute)
 
     # Saving the quotes
     with open(file_name, "w") as file:
-        for quote in quotes:
-            file.write(quote.get_text() + "\n")
+        for item in selected_elements:
+            file.write(item.get_text() + "\n")
 
-    print("Scraped quotes are saved to quotes.txt")
+    print(f"Scraped quotes are saved to {file_name}")
 else:
     print("Failed to retrieve the website")
     
